a988eabfe5e2fe22ce031073652cdcbd15f9242d
Shlyapniik <roma20053@gmail.com>
2026-04-20 16:54:22 +0300

    Double coordinate added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using System.Text.Json;
+using System.Globalization;
+using System.Linq;
 
 class WeatherResponse
 {
@@ -8,19 +10,49 @@ class WeatherResponse
 class CurrentWeather
 {
     public double temperature {get; set;}
-    public double windSpeed {get; set;}
+    public double windspeed {get; set;}
 }
 
 class Program
 {
     static async Task Main()
     {
-        Console.Write("Write latitude: ");
-        double latitude = double.Parse(Console.ReadLine());
-        Console.Write("Write longitude: ");
-        double longitude = double.Parse(Console.ReadLine());
+        Console.WriteLine("\nChoose option:\n"+
+                "1. Write latitude and logitude.\n"+
+                "2. Exit program.\n");
+        int input = int.Parse(Console.ReadLine());
 
-        await GetWeather(latitude, longitude);
+        while (input != 0)
+        {
+            Console.Clear();
+            Console.Write("Write latitude: ");
+            double latitude;
+            while (!double.TryParse(Console.ReadLine(),
+                NumberStyles.Any,
+                CultureInfo.InvariantCulture,
+                out latitude))
+            {
+                Console.Write("Invalid input. Use dot (e.g. 56.30): ");
+            }
+
+            Console.Write("Write longitude: ");
+            double longitude;
+            while (!double.TryParse(Console.ReadLine(),
+                NumberStyles.Any,
+                CultureInfo.InvariantCulture,
+                out longitude))
+            {
+                Console.Write("Invalid input. Use dot (e.g. 56.30): ");
+            }
+
+            await GetWeather(latitude, longitude); 
+
+            Console.WriteLine("\nChoose option:\n"+
+                "1. Write latitude and logitude.\n"+
+                "2. Exit program.\n");
+            input = int.Parse(Console.ReadLine());
+        }
+
     }
 
     public static async Task GetWeather(double latitude, double longitude)
@@ -36,12 +68,13 @@ class Program
             Console.WriteLine("\nRaw JSON");
             Console.WriteLine(response);
 
-            var optinons = new JsonSerializerOptions
+            var options = new JsonSerializerOptions
             {
                 TypeInfoResolver = new System.Text.Json.Serialization.Metadata.DefaultJsonTypeInfoResolver()
             };
 
-            var weather = JsonSerializer.Deserialize<WeatherResponse>(response, optinons);
+            var weatherList = JsonSerializer.Deserialize<List<WeatherResponse>>(response, options);
+            var weather = weatherList?.FirstOrDefault();
 
             if (weather?.current_weather == null)
             {
@@ -49,9 +82,10 @@ class Program
                 return;
             }
 
-            Console.WriteLine("\nParsed data:");
+            Console.WriteLine($"\nLocation: {latitude}; {longitude}");
+            Console.WriteLine("Parsed data:");
             Console.WriteLine($"Temperature: {weather.current_weather.temperature}C");
-            Console.WriteLine($"Wind speed: {weather.current_weather.windSpeed}km/h");
+            Console.WriteLine($"Wind speed: {weather.current_weather.windspeed}km/h");
         }
         catch (Exception ex)
         {
